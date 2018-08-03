b47e547de24697383fe6fe82d76ccce72b25a1fc
silver-birch-wawa <3193151055@qq.com>
2018-08-03 21:59:03 +0800

    Waiting in Line

--- 在线试题合集/Waiting in a line.cpp
+++ 在线试题合集/Waiting in a line.cpp
@@ -16,18 +16,33 @@ queue<int>waitingline;
 */
 typedef struct Windows{
   int next_time;
-  // 轮到下一个人进队的时间(秒)
+  // 轮到下一个的时间(秒)
+  int length;
+  // 队伍长度
+  int sumtime;
+  // 总计时间，防止总时长超时
+
+  // 排队的人数
   queue<int>q;
-  // 存放顾客
   Windows(){
+    sumtime=0;
+    length=0;
     next_time=0;
   }
 }Windows;
-// 填充进队列，不需要考虑超时
-void addin(Windows*win,int target,int id){
+
+bool addin(Windows*win,int target,int id){
+  // target 窗口编号 id 队伍元素编号
+  if(win[target].sumtime+k[id]>9*60){
+    return false;
+    // 处理完该业务的截至时间超过银行最晚的营业时间则报错
+  }
   win[target].q.push(id);
+  win[target].sumtime+=k[id];
+  // win[target].length++;
+  return true;
 }
-// 添加时看队伍长度，next_time只需要注册第一个人的就可以
+// 添加时看队伍长度，但是要计算时间
 void add(Windows*win,int id){
   // id 第几个顾客
   int length=1000000;
@@ -39,69 +54,91 @@ void add(Windows*win,int id){
       target=i;
     }
   }
+  // cout<<win[0].q.size()<<"           "<<win[1].q.size()<<endl;
   // 发现最小队列target
   if(length==0){
-    // cout<<"id:"<<id<<"  "<<t[id]<<endl;
-    addin(win,target,id);
     // 没人排队则初始化 next_time
     win[target].next_time+=k[id];
     t[id]=k[id];
+    // cout<<"id:"<<id<<"  "<<t[id]<<endl;
+    if(!addin(win,target,id)){
+      // 超时则不插入队列，t[i]为 0 则默认超时
+      return;
+    }
   }
   else if(length<M){
     // 有人则加入队列
-    addin(win,target,id);
+    if(!addin(win,target,id)){
+      // 超时则不插入队列，t[i]为 0 则默认超时
+      return;
+    }
   }
   else{
     // 溢出放入队列
     waitingline.push(id);
   }
 }
-// 将waitingline的顾客放入队列，把队列中的第一个顾客出队，然后标记next_time，再next_time+=
-void waitaddin(Windows*win,int target,int nextone){
+bool waitaddin(Windows*win,int target,int nextone){
+  if(win[target].sumtime+k[nextone]>9*60){
+    // cout<<"++++++"<<nextone<<endl;
+    return false;
+  }
   int out=win[target].q.front();
-  t[out]=win[target].next_time<(9*60)?win[target].next_time:0;
+  t[out]=win[target].next_time;
   win[target].q.pop();
+
   win[target].q.push(nextone);
-  // cout<<"out:"<<out<<"   "<<t[out]<<endl;
+  // cout<<"add in"<<k[win[target].q.front()]<<endl;
   win[target].next_time+=k[win[target].q.front()];
+  win[target].sumtime+=k[nextone];
+  // win[target].length++;
+  return true;
 }
 void waitadd(Windows*win){
-  int recent_time=540;
+  int recent_time=1000000;
   int i,j,target=0;
   int nextone=waitingline.front();
   waitingline.pop();
-  // 挑选最先空出来的队伍排
+  // 在一堆可能超时的数据中挑选最合适的
+  // win[target].length++;
   for(i=0;i<N;i++){
     if(recent_time>win[i].next_time){
       recent_time=win[i].next_time;
       target=i;
     }
   }
-  waitaddin(win,target,nextone);
+  if(recent_time==1000000){
+    //cout<<"???????";
+    return;
+    // 都超时了
+  }
+  if(!waitaddin(win,target,nextone)){
+    return;
+  }
+  // t[out]=recent_time+k[nextone];
+  // 把该人的完成时间标记
 }
-// 当 waitingline清空了，就把
 void clearqueue(Windows*win){
   int i,j;
   // cout<<"***next_time:"<<win[0].q.size()<<"  "<<win[1].q.size()<<endl;
   for(i=0;i<N;i++){
     int length=win[i].q.size();
-        if(length>0){
-          int first=win[i].q.front();
-          win[i].q.pop();
-          t[first]=win[i].next_time-k[first]<9*60?win[i].next_time:0;
-          // 处理后面的顾客
-          for(j=1;j<length;j++){
-            int first=win[i].q.front();
-            win[i].q.pop();
-            // cout<<"next_time:"<<win[i].next_time<<endl;
-            t[first]=win[i].next_time<9*60?win[i].next_time+k[first]:0;
-            win[i].next_time+=k[first];
-            // cout<<"q:"<<i<<endl;
-            // cout<<"first:"<<first<<endl;
-            // win[i].q.pop();
-          }
-        }
+    if(length>0){
+      int first=win[i].q.front();
+      win[i].q.pop();
+      t[first]=win[i].next_time;
+      for(j=1;j<length;j++){
+        int first=win[i].q.front();
+        win[i].q.pop();
+        win[i].next_time+=k[first];
+        t[first]=win[i].next_time;
+        //cout<<"q:"<<i<<endl;
+        //cout<<"first:"<<first<<endl;
+        //cout<<"next_time:"<<t[first]<<endl;
+        // win[i].q.pop();
+      }
     }
+  }
 }
 void print(){
   int i=0;
@@ -118,7 +155,8 @@ void times(int t){
   }
   int hour=8+t/60;
   int minutes=(t%60);
-  printf("%02d:%02d\n",hour,minutes);
+  cout<<hour<<":";
+  printf("%02d\n",minutes);
 }
 void query(int num){
   times(t[num-1]);
@@ -133,32 +171,19 @@ int main(){
     add(win,i);
     // 插入
   }
-  // 如果黄线之前排满了，waitingline
+  // cout<<"***next_time:"<<win[0].next_time<<"  "<<win[1].next_time<<endl;
+  // print();
+  // 如果黄线之前排满了
   int length=waitingline.size();
   for(i=0;i<length;i++){
     waitadd(win);
   }
-  // waitingline全部都入队了，对队伍中的最后一批顾客进行清算
+  //print();
+  // 全部都入队了，对队伍中的最后一批顾客进行清算
   clearqueue(win);
-  // print();
+  //print();
   for(i=0;i<Q;i++){
     scanf("%d",&input);
     query(input);
   }
 }
-/*
-2 2 7 5
-1 2 6 4 3 534 2
-3 4 5 6 7
-08:07
-08:06
-08:10
-17:00
-Sorry
-
-
-2 2 9 9
-1 2 6 4 3 1 9 100 534
-1 2 3 4 5 6 7 8 9
-08:01 08:02 08:07 08:06 08:10 08:07 08:16 09:50 17:10
-*/
