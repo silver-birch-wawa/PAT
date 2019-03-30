4325dbeb5543dd786f0492883783d2f0a43182e4
silver-birch-wawa <3193151055@qq.com>
2019-03-30 12:18:30 +0800

    add 回文

--- 算法学习/动态规划/最长回文子串/最长回文子串(Manacher法).cpp
+++ 算法学习/动态规划/最长回文子串/最长回文子串(Manacher法).cpp
@@ -1,78 +1,69 @@
-#include <bits/stdc++.h>
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<map>
 using namespace std;
-#define N 1000
-char input[N],a[N];
-vector<vector<int>>v;
-int length;
-// 记录字符长度
+#define N 100
+int mark[N]={0};
+// 串的半径 @#a#a#a#*的半径为3，半径等于a的数量 @#a#a#*的半径为2，半径等于a的数量
 
-int max_length=0;
-//记录最大长度
+int ans=0;
+// 记录最长串长
 
-// 记录回文产生的起点与终点
-void record(int start,int end){
-  if(max_length<(end-start+1)/2){
-    max_length=(end-start+1)/2;
-  }
-  v[start].push_back(end);
-}
-void show(int start,int end){
-  int i;
-  cout<<"\nstart from:"<<start<<"\nend at:"<<end<<endl;
-  for(i=start;i<=end;i++){
-    if(input[i]!='#')cout<<input[i]<<">>";
-  }
-  cout<<endl;
-}
-void Single(char input[],int middle){
-  int i,j;
-  // 要不往左middle次迭代，要不往右length-1-middle次迭代
-  for(i=1;i<=min(middle,length-1-middle);i++){
-    if(input[middle-i]==input[middle+i]){
-
-    }else{
-      break;
+void check(vector<int>&v,int num){
+    if(mark[num]!=0){
+        // 半径已经确定直接返回
+        return;
     }
-  }
-  if(i>2){
-    // 考虑到 d#b#a#b#x(i>=4) x#b#b#y(i>=3)，所以i>2
-    record(middle-i+1,middle+i-1);
-  }
+    int j=0,i=1,len=min(num,int(v.size())-1-num);
+    // len 为最大可能半径
+    while(i<len+1){
+        if(v[num+i]!=v[num-i]){
+            break;
+        }
+        i++;
+    }
+    // 因为在两端插入了@和*，所以避免了2222全匹配然后未经过break的情况（之前把赋值放在break里面然后没执行）
+    mark[num]=i-1;
+    ans=max(mark[num],ans);
+    for(j=1;j<=mark[num];j++){
+            mark[num+j]=min(mark[num-j],len-j);
+            // 当p2的右边界跟mark[num]+num重合时，manacher算法只能帮你算出p2半径的最小可能，实际可能比len-j更大
+            // 不重复的话就是mark[num-j]/len-j
+            if(j+mark[num+j]==mark[num]){
+                while(v[num+j+mark[num+j]+1]==v[num+j-mark[num+j]-1]){mark[num+j]++;}
+            }
+            ans=max(mark[num+j],ans);
+    }
 }
 int main(){
-  int i,j;
-  scanf("%s",&a);
-  input[0]='#';
-  for(i=0;i<strlen(a);i++){
-    input[2*i+1]=a[i];
-    input[2*i+2]='#';
-  }
-  input[strlen(a)*2+1]='\0';
-  length=strlen(input);
-
-  for(i=0;i<length;i++){
-    vector<int>vv;
-    v.push_back(vv);
-  }
-  for(i=0;i<length;i++){
-    //if(input[i]!='#')
-    // middle遇到#也要算进去，这样可以少处理偶数的情况
-    Single(input,i);
-  }
-  for(i=0;i<length;i++){
-    if(!v[i].empty()){
-      for(j=0;j<v[i].size();j++){
-        show(i,v[i][j]);
-      }
+    int i=0;
+    vector<int>v;
+    i=getchar();
+    v.push_back('@');
+    v.push_back('#');
+    v.push_back(i);
+    while(1){
+        i=getchar();
+        if(i=='\n'){
+            break;
+        }
+        v.push_back('#');
+        v.push_back(i);
     }
-  }
-  cout<<"\nmax_length:"<<max_length<<endl;
+    v.push_back('#');
+    v.push_back('*');
+    for(i=0;i<v.size();i++){
+        check(v,i);
+    }
+    cout<<ans;
 }
 
+
 /*
-abcdcbadedab
+cabadabae
+内包对称
 
-abcdcbadedababbccbbavenne
-
-abbccbbavenne
+abacabe
+外突左对称右不一定
 */
